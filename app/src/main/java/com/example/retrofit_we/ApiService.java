e4ed36a7d49e1e65dc27ff6b08d56ac2465afc29
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-15 19:20:16 +0530

    Files Added

---
+++ app/src/main/java/com/example/retrofit_we/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.retrofit_we;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiService {
+
+   @GET("/api/users/{userId}")
+   Call<ResponseModel> getUser(@Path("userId") int userId);
+
+}
